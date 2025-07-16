4cc8b284fee9fe2d04a362dc2fb533305bfd8c6e
andressamaf <aasf93@hotmail.es>
2025-07-16 08:59:47 -0500

    fix: add url to redirect to cas login

--- src/app/modules/register/components/final-form/final-form.component.ts
+++ src/app/modules/register/components/final-form/final-form.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-final-form',
   templateUrl: './final-form.component.html',
 })
 export class FinalFormComponent {
+
+  url = environment.casLogin;
   constructor() {}
 }
